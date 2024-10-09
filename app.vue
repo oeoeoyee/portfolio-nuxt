3b18207b536b90f457f60abf1f58a4eb3f87fadc
oeoeoyee <polly931621@gmail.com>
2024-10-09 21:58:03 +0800

    202410092156-初次建立專案。已有基礎套件安裝設置、嘗試一點tailwind寫法、導覽頁寫法

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="font-mono">
+    <ul class="flex flex-row border-solid	border-black border hover:border-slate-100"> 
+        <li class="flex-auto"><NuxtLink to="/">Index</NuxtLink></li>
+        <li class="flex-auto"><NuxtLink to="/about">About</NuxtLink></li>
+    </ul>
+    <!-- <NuxtRouteAnnouncer /> -->
+    <!-- <NuxtWelcome /> -->
+    <NuxtLayout>
+      <NuxtPage />
+      <h1 class="text-6xl py-10">首頁/目錄</h1>
+      <p>本專案(應該會)使用 Vue3、Vite、Nuxt、TypeScript、tailwind、JavaScript、CSS、HTML</p>
+    </NuxtLayout>
+<!--     <div>
+      <AppAlert>
+        This is an auto-imported component.
+      </AppAlert>
+    </div> -->
+  </div>
+</template>
+
+<style>
+@import 'tailwindcss/tailwind.css';
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.{vue,js,ts}',
+    './pages/**/*.{vue,js,ts}',
+    './composables/**/*.{vue,js,ts}',
+    './plugins/**/*.{vue,js,ts}',
+    './App.{js,ts,vue}',
+    './app.{js,ts,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
