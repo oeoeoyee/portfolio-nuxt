8851716f96ca12587a659f19ddb1865b9109a456
oeoeoyee <oeoeoyee@users.noreply.github.com>
2024-12-12 16:28:12 +0000

    deploy: af93eb003b221c68b9b0f540d9fe903a2a0f091a

--- _nuxt/Dr0VljYN.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as t,K as c,o}from"./5Lp_Gvtp.js";const n={},r={class:"min-h-screen"};function a(e,_){return o(),t("div",r,[c(e.$slots,"default")])}const d=s(n,[["render",a]]);export{d as default};
